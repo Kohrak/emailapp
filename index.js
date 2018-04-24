9d9d3852fefc4b1ce38b33720edb3656a8329f8a
Kohrak <gabrielgt95@gmail.com>
2018-04-24 21:52:18 +0100

    add cookies and session

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express'),
       mongoose = require('mongoose'),
       keys = require('./config/keys'),
-      app = express();
+      cookieSession = require('cookie-session'),
+      passport = require('passport');
 
-mongoose.connect(keys.mongoURI);
 require('./models/User');
 require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
